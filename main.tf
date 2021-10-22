ed44063e0100797cd3c61963ca1126607244cf95
amccaskill <mccaskill.adrian@gmail.com>
2021-10-22 10:46:19 -0400

    Format Updated

--- main.tf
+++ main.tf
@@ -1,23 +1,39 @@
 terraform {
   required_providers {
     aci = {
-      source = "CiscoDevNet/aci"
-      version = "0.7.1"
+      source = "Ciscodevnet/aci"
     }
   }
 }
 
 provider "aci" {
-  # Configuration options
+  username = "admin"
+  password = "C1sco12345"
+  url      = "https://10.10.20.14"
+  insecure = true
 }
 
-# Configure the provider with your Cisco APIC credentials.
-provider "aci" {
-  # APIC Username
-  username = var.user.username
-  # APIC Password
-  password = var.user.password
-  # APIC URL
-  url      = var.user.url
-  insecure = true
+resource "aci_tenant" "terraform_tenant" {
+       name          = "am_34761"
+       description = "This tenant is created by terraform"
+}
+# Define an ACI Tenant VRF Resource.
+resource "aci_vrf" "terraform_vrf" {
+     tenant_dn       = aci_tenant.terraform_tenant.id
+     description     = "VRF Created Using Terraform"
+     name              = "adrian_vrf"
+}
+# Define an ACI Tenant BD Resource
+resource "aci_bridge_domain" "terraform_bd" {
+    tenant_dn          = aci_tenant.terraform_tenant.id
+    relation_fv_rs_ctx = aci_vrf.terraform_vrf.id
+    description        = "BD Created Using Terraform"
+    name               = "adrian_bd"
+}
+# Define an ACI Tenant BD Subnet Resource.
+resource "aci_subnet" "terraform_bd_subnet" {
+     parent_dn     = aci_bridge_domain.terraform_bd.id
+     description   = "Subnet Created Using Terraform"
+     ip                  = "10.10.20.40/16"
+     scope            = ["public"]
 }
